dffe99f838f52601cd99b59b8b394ba444772057
CRTMCQ <146783107+CRTMCQ@users.noreply.github.com>
2024-06-09 22:44:23 -0700

    add citation for db-connector.js

--- database/db-connector.js
+++ database/db-connector.js
@@ -1,4 +1,9 @@
-// ./database/db-connector.js
+// Citation for the following code:
+// Date: 5/23/2024
+// Adapted from: CS340 nodejs-starter-app 
+// All code below was copied from the nodejs-starter-app db-connector.js, and is
+//     modified to use a particular username, password, and database.
+// Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app
 
 // Get an instance of mysql we can use in the app
 var mysql = require('mysql')
